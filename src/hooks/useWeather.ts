e9d9d27772694aaa6e16521b3f3f612b0f788295
misterdan100 <danielmca60@gmail.com>
2024-09-10 07:58:49 -0500

    fix: add https to geoUrl

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -43,7 +43,7 @@ export default function useWeather() {
         setWeather(initialState)
         try {
 
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`
             const { data } = await axios(geoUrl)
 
             // check if city exist
